960f4f8514aebfa8cfd736ccd32f04ef3a9d69e6
Subham-WH-GT <awandboy685@gmail.com>
2024-07-31 22:09:24 +0530

    updated

--- Rest/api/views.py
+++ Rest/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from .models import PlayerModel 
 from .serializers import PlayerSerializer 
@@ -7,5 +9,7 @@ from .serializers import PlayerSerializer
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset=PlayerModel.objects.all()
     serializer_class=PlayerSerializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
 
 #Super_user user and pass : Subham    
